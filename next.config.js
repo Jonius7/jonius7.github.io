9d14b6f448583c042ed59deb57418219446a1e6c
Jonius7 <jhgenius01@hotmail.com>
2025-08-29 09:59:12 +1000

    add next export config

--- next.config.js
+++ next.config.js
@@ -15,4 +15,7 @@ if (isGithubActions) {
 module.exports = {
   assetPrefix: assetPrefix,
   basePath: basePath,
+  output: 'export', // enables next export
+  basePath: '/jonius7.github.io', // replace with your repo name
+  trailingSlash: true, // optional, better for static hosting
 }
