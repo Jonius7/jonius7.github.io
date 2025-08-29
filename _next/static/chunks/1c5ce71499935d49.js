7f642217c0ff09663815c2004fc582c2f9b46731
Jonius7 <jhgenius01@hotmail.com>
2025-08-29 00:07:54 +0000

    Deploying to gh-pages from @ Jonius7/jonius7.github.io@2be0e361b63110c6a56169ce44d9cea87ffddadf 🚀

---
+++ _next/static/chunks/1c5ce71499935d49.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/0126b40f42ba361e.js",
+  "static/chunks/turbopack-be382c73a9c90402.js"
+])
---
+++ _next/static/r_XW6hbVFVvL0IrBOGH1P/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/db15ec5e4b744d89.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/jonius7.github.io//_next/:path+",
+        "destination": "/jonius7.github.io/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
