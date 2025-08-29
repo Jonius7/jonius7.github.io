a9595407e2aabc89c280aa751ef11e011eb86c16
Jonius7 <jhgenius01@hotmail.com>
2025-08-29 12:52:50 +1000

    re unroot

--- next.config.js
+++ next.config.js
@@ -6,18 +6,18 @@ let assetPrefix = ''
 let basePath = ''
 
 if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  // If deploying to <username>.github.io (root), leave them empty
-  //if (repo !== `${process.env.GITHUB_REPOSITORY.split('/')[0]}.github.io`) {
-  //  assetPrefix = `/${repo}/`
-  //  basePath = `/${repo}`
-  //}
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+    
+    //If deploying to <username>.github.io (root), leave them empty
+    if (repo !== `${process.env.GITHUB_REPOSITORY.split('/')[0]}.github.io`) {
+        assetPrefix = `/${repo}/`
+        basePath = `/${repo}`
+    }
 }
 
 module.exports = {
-  assetPrefix,
-  basePath,
-  output: 'export',      // required for npm run export
-  trailingSlash: true,   // good for static hosting
+    assetPrefix,
+    basePath,
+    output: 'export',      // required for npm run export
+    trailingSlash: true,   // good for static hosting
 }
