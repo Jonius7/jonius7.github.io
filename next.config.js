e4a2ce404bb8438d746b50f29353b50abde09dfb
Jonius7 <jhgenius01@hotmail.com>
2025-08-29 12:57:42 +1000

    un re unroot

--- next.config.js
+++ next.config.js
@@ -9,10 +9,10 @@ if (isGithubActions) {
     const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
     
     //If deploying to <username>.github.io (root), leave them empty
-    if (repo !== `${process.env.GITHUB_REPOSITORY.split('/')[0]}.github.io`) {
-        assetPrefix = `/${repo}/`
-        basePath = `/${repo}`
-    }
+    //if (repo !== `${process.env.GITHUB_REPOSITORY.split('/')[0]}.github.io`) {
+    //    assetPrefix = `/${repo}/`
+    //    basePath = `/${repo}`
+    //}
 }
 
 module.exports = {
